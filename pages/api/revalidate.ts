42486f9ecf5cfcb07b8d1045f83008778afa8e18
ElyasAmri <elyas.amri@gmail.com>
2022-05-09 09:24:46 +0300

    chore: add revalidate API

---
+++ pages/api/revalidate.ts
@@ -0,0 +1,22 @@
+import type {NextApiRequest, NextApiResponse} from 'next'
+
+type Data = {
+  message: string,
+  revalidated: boolean
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>,
+) {
+  if (req.query.secret !== process.env.REVALIDATE_TOKEN) {
+    return res.status(401).json({message: 'Invalid token', revalidated: false})
+  }
+
+  try {
+    await res.unstable_revalidate(`/${req.query.path}`)
+    return res.json({message: 'Revalidated successfully', revalidated: true})
+  } catch (err) {
+    return res.status(500).send({message: 'Error revalidating', revalidated: false})
+  }
+}
