7fa5698df7ff063c8899b2588c5c76e6ff5d1ebd
ElyasAmri <elyas.amri@gmail.com>
2022-05-17 20:07:06 +0300

    chore: add schema

---
+++ prisma/index.ts
@@ -0,0 +1,7 @@
+import {PrismaClient} from '@prisma/client'
+
+const prisma = process.env.NODE_ENV === 'production' || !('prisma' in global) ?
+    new PrismaClient() :
+    (global as any).prisma as PrismaClient
+
+export {prisma}
