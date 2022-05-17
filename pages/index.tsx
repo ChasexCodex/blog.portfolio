56fcde44f3f5559523d36da5100aceb6c934f716
ElyasAmri <elyas.amri@gmail.com>
2022-05-17 20:36:14 +0300

    code: index static props use prisma instead of supabase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,40 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import {getPosts} from '../data/posts'
 
 import type {GetStaticProps, NextPage} from 'next'
 import {PostBanner} from '../components'
 import {Post} from '../types'
+import {prisma} from '../prisma'
+
+const postsCount = 10
 
 export const getStaticProps: GetStaticProps = async () => {
-  const posts = await getPosts()
+  const posts = (await prisma.post.findMany({
+    take: postsCount,
+    orderBy: {created_at: 'asc'},
+  })).map(post => ({
+    ...post,
+    created_at: JSON.stringify(post.created_at),
+    updated_at: JSON.stringify(post.updated_at),
+  }))
 
   return {
     props: {
-      posts
+      posts,
     },
     revalidate: 60,
   }
 }
 
 type Props = {
-  posts: any
+  posts: Post[]
 }
 
 const Main: NextPage<Props> = ({posts}) => {
   return (
       <div className={styles.container}>
         <Head>
-          <title>Elyas Al-Amri{"'"}s Blog</title>
+          <title>Elyas Al-Amri{'\''}s Blog</title>
         </Head>
         <div>
           {posts.map((post: Post) => <PostBanner key={post.id} post={post}/>)}
