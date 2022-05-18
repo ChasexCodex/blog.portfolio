1006fdfe5a85d986f3a5cb9f5f5701b28991c91c
ElyasAmri <elyas.amri@gmail.com>
2022-05-18 15:51:40 +0300

    design: footer

--- components/FooterInfo.tsx
+++ components/FooterInfo.tsx
@@ -1,16 +1,22 @@
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Footer.module.css'
 
 const FooterInfo = () => {
   return (
-      <footer>
-        <p>Made by Elyas</p>
-        <p>
-          <span>Powered By</span>
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16}/>
-          </span>
-        </p>
+      <footer className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.text}>
+            <p className={styles.maker}>Made by Elyas</p>
+          </div>
+          <div className={styles.text}>
+            <p className={styles.org}>
+              <span>Powered By</span>
+              <span className={styles.logo}>
+                <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16}/>
+              </span>
+            </p>
+          </div>
+        </div>
       </footer>
   )
 }
