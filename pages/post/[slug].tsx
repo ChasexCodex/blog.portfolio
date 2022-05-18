bcbcb576c45b970047a1d5aed23db175912fae79
ElyasAmri <elyas.amri@gmail.com>
2022-05-18 17:04:19 +0300

    code: pass mdx page props

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,6 +1,15 @@
 import {GetStaticPaths, GetStaticProps} from 'next'
 import {prisma} from '../../prisma'
 import {Post} from '../../types'
+import {serialize} from 'next-mdx-remote/serialize'
+import matter from 'gray-matter'
+import {MDXRemote, MDXRemoteSerializeResult} from 'next-mdx-remote'
+
+import rehypeHighlight from 'rehype-highlight'
+import rehypeCodeTitles from 'rehype-code-titles'
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const res = await prisma.post.findMany({
@@ -35,25 +44,46 @@ export const getStaticProps: GetStaticProps<any, {slug: string}> = async ({param
   post.created_at = JSON.stringify(post.created_at)
   post.updated_at = JSON.stringify(post.updated_at)
 
+  const {content} = matter(post.content)
+  const source = await serialize(content, {
+    mdxOptions: {
+      rehypePlugins: [
+        rehypeSlug,
+        [
+          rehypeAutolinkHeadings,
+          {
+            properties: {className: ['anchor']},
+          },
+          {behaviour: 'wrap'},
+        ],
+        rehypeHighlight,
+        rehypeCodeTitles,
+      ],
+    },
+  })
+
   return {
     props: {
       post,
+      source,
     },
   }
 }
 
 type Props = {
   post: Post | any
+  source: MDXRemoteSerializeResult
 }
 
-const Post = ({post}: Props) => {
+const Post = ({post, source}: Props) => {
   return (
       <div>
         <p>{post.title}</p>
-        <img src={post.thumbnail} alt={post.title + ' image'}/>
+        <img src={post.thumbnail ?? 'https://picsum.photos/400/400'} alt={post.title + ' image'}/>
         <p>{post.description}</p>
-        <p>{post.author} / {post.created_at} / {post.updated_at}</p>
-        <p>{post.content}</p>
+        <article>
+          <MDXRemote {...source}/>
+        </article>
       </div>
   )
 }
