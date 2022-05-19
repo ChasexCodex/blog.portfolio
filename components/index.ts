53643cc9ea6dd6422fef87c92569132ad0605794
ElyasAmri <elyas.amri@gmail.com>
2022-05-19 16:26:20 +0300

    design: couple components if they are used once

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,1 @@
-export {default as NavBar} from './NavBar'
-export {default as FooterInfo} from './FooterInfo'
 export {default as PostBanner} from './PostBanner'
--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -1,5 +1,40 @@
 import {ReactElement} from 'react'
-import {FooterInfo, NavBar} from '../components'
+import Image from 'next/image'
+
+const FooterInfo = () => {
+  return (
+      <footer>
+        <div>
+          <div>
+            <p>Made by Elyas</p>
+          </div>
+          <div>
+            <p>
+              <span>Powered By</span>
+              <span>
+                <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16}/>
+              </span>
+            </p>
+          </div>
+        </div>
+      </footer>
+  )
+}
+
+const NavBar = () => {
+  return (
+      <nav>
+        <a href={process.env.APP_URL}>
+          Elyas Al-Amri{"'"}s Blog
+        </a>
+        <div>
+          <a href={process.env.NEXT_PUBLIC_PORTFOLIO_URL}>
+            Portfolio
+          </a>
+        </div>
+      </nav>
+  )
+}
 
 const MainLayout = (page: ReactElement) => {
   return (
