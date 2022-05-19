2a7353a6d2b60d1a55a68624005e87a2b63ee79b
ElyasAmri <elyas.amri@gmail.com>
2022-05-19 18:04:45 +0300

    design: post

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -10,6 +10,8 @@ import rehypeCodeTitles from 'rehype-code-titles'
 import rehypeSlug from 'rehype-slug'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 
+import 'github-markdown-css'
+
 export const getStaticPaths: GetStaticPaths = async () => {
   const res = await prisma.post.findMany({
     select: {slug: true},
@@ -76,20 +78,20 @@ type Props = {
 
 const Post = ({post, source}: Props) => {
   return (
-      <div>
-        <div>
-          <div>
-            <img src={post.thumbnail ?? 'https://picsum.photos/400/400'} alt={post.title + ' image'}/>
-            <div>
-              <p>{post.title}</p>
-              <p>{post.description ?? 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias commodi eum in iste laudantium placeat quaerat quas totam vero voluptates.'}</p>
-            </div>
+      <div className="flex flex-col max-w-5xl mx-auto my-4">
+        <div className="flex flex-row mb-4">
+          <img src={post.thumbnail ?? 'https://picsum.photos/400/400'} alt={post.title + ' image'}/>
+          <div className="flex flex-col px-4 py-2">
+            <p className="text-4xl font-semibold">{post.title}</p>
+            <p className="pt-4">{post.description ?? 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias commodi eum in iste laudantium placeat quaerat quas totam vero voluptates.'}</p>
+            <div className="flex-1"/>
+            <p className="">{post.created_at}</p>
+            <p className="">{post.updated_at}</p>
           </div>
-          <div/>
-          <article>
-            <MDXRemote {...source}/>
-          </article>
         </div>
+        <article className="markdown-body markdown py-2 px-8">
+          <MDXRemote {...source}/>
+        </article>
       </div>
   )
 }
