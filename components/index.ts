1738c84a09bc033bb6a6414498ffab760cd020ee
ElyasAmri <elyas.amri@gmail.com>
2022-05-19 17:36:37 +0300

    design: home

--- components/index.ts
+++
@@ -1,1 +0,0 @@
-export {default as PostBanner} from './PostBanner'
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
 import Head from 'next/head'
-
 import type {GetStaticProps, NextPage} from 'next'
-import {PostBanner} from '../components'
 import {Post} from '../types'
 import {prisma} from '../prisma'
+import Link from 'next/link'
 
 const postsCount = 10
 
@@ -36,21 +35,47 @@ export const getStaticProps: GetStaticProps = async () => {
   }
 }
 
+
+const Banner = ({post}: any) => {
+  return (
+      <div className="flex flex-row bg-white shadow-md">
+        <div className="mr-auto p-4 flex flex-col">
+          <Link href={`/posts/${post.slug}`}>
+            <a className="text-2xl">{post.title}</a>
+          </Link>
+
+          {post.author !== process.env.NEXT_PUBLIC_DEFAULT_AUTHOR &&
+            <p>{post.author}</p>
+          }
+          <div className="mt-auto">
+            <p>Published: {post.created_at}</p>
+            {post.updated_at !== post.created_at &&
+              <p>Last Edit: {post.updated_at}</p>
+            }
+          </div>
+        </div>
+        <img src={post.thumbnail ?? 'https://picsum.photos/400/400'} alt={post.title}/>
+      </div>
+  )
+}
+
 type Props = {
   posts: Post[]
 }
 
-const Main: NextPage<Props> = ({posts}) => {
+const Home: NextPage<Props> = ({posts}) => {
   return (
-      <div>
-        <Head>
-          <title>Elyas Al-Amri{'\''}s Blog</title>
-        </Head>
-        <div>
-          {posts.map(post => <PostBanner key={post.id} post={post}/>)}
+      <div className="w-full flex flex-col items-center">
+        <div className="w-full max-w-6xl my-4">
+          <Head>
+            <title>Elyas Al-Amri{'\''}s Blog</title>
+          </Head>
+          <div className="grid grid-column-1 gap-y-4">
+            {posts.map(post => <Banner key={post.id} post={post}/>)}
+          </div>
         </div>
       </div>
   )
 }
 
-export default Main
+export default Home
