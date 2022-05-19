d9459c3a12050ca0d7dfe25730d9ef0180ad571c
ElyasAmri <elyas.amri@gmail.com>
2022-05-19 18:40:25 +0300

    ux: add links

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -1,12 +1,15 @@
 import {ReactElement} from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const FooterInfo = () => {
   return (
       <footer className="h-32 flex flex-row bg-gray-900 px-4 space-x-4 mt-4">
         <div className="flex-1 flex flex-row items-center justify-evenly my-4 px-4 text-2xl text-white font-semibold">
-          <a>Posts</a>
-          <a>Portfolio</a>
+          <Link href="/posts/page/1"><a>Posts</a></Link>
+          <a href={process.env.NEXT_PUBLIC_PORTFOLIO_URL}>
+            Portfolio
+          </a>
         </div>
         <div className="bg-white flex flex-row justify-center items-center space-x-8 py-4 px-8 my-4">
           <p className="text-xl font-semibold italic">Made by Elyas A. Al-Amri</p>
@@ -22,10 +25,11 @@ const FooterInfo = () => {
 const NavBar = () => {
   return (
       <nav className="h-20 shadow-md flex flex-row items-center px-4">
-        <a className="mr-auto text-4xl font-bold"
-           href={process.env.APP_URL}>
-          Elyas Al-Amri{'\''}s Blog
-        </a>
+        <Link href="/">
+          <a className="mr-auto text-4xl font-bold">
+            Elyas Al-Amri{'\''} s Blog
+          </a>
+        </Link>
         <a className="text-2xl font-extrabold"
            href={process.env.NEXT_PUBLIC_PORTFOLIO_URL}>
           Portfolio
