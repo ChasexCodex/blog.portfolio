70fbf9508efd64d48156354a0d959f7d5998a1be
ElyasAmri <elyas.amri@gmail.com>
2022-06-01 18:45:49 +0300

    seo: remove unnecessary anchors

--- components/CategoryLabel.tsx
+++ components/CategoryLabel.tsx
@@ -7,11 +7,11 @@ type Props = {
 const CategoryLabel = ({category}: Props) => {
 	// TODO: make label clickable
 	return (
-		<a className="inline-block max-w-max bg-orange-400 px-1.5 rounded-full font-medium text-white text-2xs
+		<span className="inline-block max-w-max bg-orange-400 px-1.5 rounded-full font-medium text-white text-2xs
 									xl:text-xs xl:py-0.5 xl:font-semibold bg-rose-700
 									">
 			{category.name}
-		</a>
+		</span>
 	)
 }
 
--- components/TagList.tsx
+++ components/TagList.tsx
@@ -9,12 +9,12 @@ const TagList = ({tags}: Props) => {
 	return (
 		<div className="inline-block space-x-1">
 			{tags.map(t => (
-				<a key={t.id}
+				<span key={t.id}
 					 className="inline-block bg-green-500 px-1 transform rounded-full text-2xs font-medium text-white
 					 						xl:text-xs xl:px-1.5 xl:py-0.5 xl:font-semibold bg-green-700
 					 						">
 					{t.name}
-				</a>
+				</span>
 			))}
 		</div>
 	)
