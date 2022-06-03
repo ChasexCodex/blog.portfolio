e0cb642124d399e1bdc87d17ed7d9207d2fe40e3
ElyasAmri <elyas.amri@gmail.com>
2022-06-03 11:43:05 +0300

    ux: require all fields in contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -26,30 +26,32 @@ const ContactPage: NextPage = () => {
 					className="flex-1 flex flex-col py-4 space-y-2 mx-auto w-full max-w-5xl">
 			<div>
 				<label htmlFor="name" className="block mr-2 mb-1 dark:text-white">Name</label>
-				<input type="text" placeholder="Your Name..."
+				<input type="text" placeholder="Your Name..." required
 							 className="block w-full px-1.5 py-1 rounded-sm bg-zinc-300 shadow dark:shadow-neutral-400"/>
 			</div>
+
 			<div>
 				<label htmlFor="email" className="block mr-2 mb-1 dark:text-white">Email</label>
-				<input type="email" className="block w-full px-1.5 py-1 rounded-sm bg-zinc-300 shadow dark:shadow-neutral-400"
-							 placeholder="Your Email..."/>
+				<input type="email" required placeholder="Your Email..."
+							 className="block w-full px-1.5 py-1 rounded-sm bg-zinc-300 shadow dark:shadow-neutral-400"/>
 			</div>
+
 			<div>
 				<label htmlFor="subject" className="block mr-2 mb-1 dark:text-white">Subject</label>
-				<input type="text" className="block w-full px-1.5 py-1 rounded-sm bg-zinc-300 shadow dark:shadow-neutral-400"
-							 placeholder="Subject..."/>
+				<input type="text" required placeholder="Subject..."
+							 className="block w-full px-1.5 py-1 rounded-sm bg-zinc-300 shadow dark:shadow-neutral-400"/>
 			</div>
+
 			<div className="flex-1 flex flex-col">
 				<label htmlFor="details" className="block mr-2 mb-1 dark:text-white">Details</label>
-				<textarea name="details" placeholder="Describe your matter..."
-									className="block w-full px-1.5 py-1 rounded-sm bg-zinc-300 shadow dark:shadow-neutral-400
-														 flex-1"/>
+				<textarea name="details" required placeholder="Describe your matter..."
+									className="block w-full px-1.5 py-1 rounded-sm bg-zinc-300 shadow dark:shadow-neutral-400 flex-1"/>
 			</div>
+
 			<input type="submit" value="Submit"
 						 className="block text-white w-max font-bold px-4 py-1 mx-auto transition shadow outline outline-2 outline-white
 												hover:bg-black hover:text-white
-												dark:hover:bg-white dark:hover:text-black
-												"/>
+												dark:hover:bg-white dark:hover:text-black"/>
 		</form>
 	)
 }
