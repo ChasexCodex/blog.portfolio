525b863b1bccdb69f373a41249e5f97220070b75
ElyasAmri <elyas.amri@gmail.com>
2022-06-03 14:30:51 +0300

    lint: additional components with no className end on new line

--- components/CategoryLabel.tsx
+++ components/CategoryLabel.tsx
@@ -8,8 +8,7 @@ const CategoryLabel = ({category}: Props) => {
 	// TODO: make label clickable
 	return (
 		<span className="inline-block max-w-max bg-orange-400 px-1.5 rounded-full font-medium text-white text-2xs
-									xl:text-xs xl:py-0.5 xl:font-semibold bg-rose-700
-									">
+										 xl:text-xs xl:py-0.5 xl:font-semibold bg-rose-700">
 			{category.name}
 		</span>
 	)
--- components/TagList.tsx
+++ components/TagList.tsx
@@ -10,9 +10,8 @@ const TagList = ({tags}: Props) => {
 		<div className="inline-block space-x-1">
 			{tags.map(t => (
 				<span key={t.id}
-					 className="inline-block bg-green-500 px-1 transform rounded-full text-2xs font-medium text-white
-					 						xl:text-xs xl:px-1.5 xl:py-0.5 xl:font-semibold bg-green-700
-					 						">
+							className="inline-block bg-green-500 px-1 transform rounded-full text-2xs font-medium text-white
+												 xl:text-xs xl:px-1.5 xl:py-0.5 xl:font-semibold bg-green-700">
 					{t.name}
 				</span>
 			))}
